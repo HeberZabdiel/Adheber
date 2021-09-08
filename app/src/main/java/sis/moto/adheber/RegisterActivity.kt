ddfad241a6d5689e0fa64b624607f6fb191bd705
HeberZabdiel <hzhm1997@gmail.com>
2021-09-08 03:06:07 -0500

    creacion EmailPasswordActivity.kt

--- app/src/main/java/sis/moto/adheber/RegisterActivity.kt
+++ app/src/main/java/sis/moto/adheber/RegisterActivity.kt
@@ -3,14 +3,17 @@ package sis.moto.adheber
 //import android.support.v7.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var emailPasswordActivity: EmailPasswordActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-    }
-    fun registrarUsuario(username: String, password: String)
-    {
+        emailPasswordActivity.onCreate(savedInstanceState)
 
     }
+
 }
